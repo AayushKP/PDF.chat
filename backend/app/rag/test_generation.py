09a02cc543527052633ce63d729de510192696cf
AayushKP <ayushprokashyap@gmail.com>
2026-07-28 23:19:19 +0530

    test generation of llm with contexts

---
+++ backend/app/rag/test_generation.py
@@ -0,0 +1,11 @@
+from app.rag.generation import generate_answer
+
+answer = generate_answer("What is a Forward Deployed AI Engineer?")
+
+print()
+
+print("=" * 80)
+
+print(answer)
+
+print("=" * 80)
