146f09dbee914dd47f9a2e178a06a64cf64eaa39
AayushKP <ayushprokashyap@gmail.com>
2026-07-30 23:02:47 +0530

    update schema

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -47,7 +47,10 @@ class User(Base):
 
     name: Mapped[str] = mapped_column(String)
 
-    picture: Mapped[str] = mapped_column(String)
+    picture: Mapped[str | None] = mapped_column(
+        String,
+        nullable=True,
+    )
 
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
