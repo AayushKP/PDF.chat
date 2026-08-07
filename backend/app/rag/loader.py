53f51032e24f2992657063b6d1c20e9c5e6c9d06
AayushKP <ayushprokashyap@gmail.com>
2026-08-08 01:11:25 +0530

    add time for performance measure

--- backend/app/rag/loader.py
+++ backend/app/rag/loader.py
@@ -1,13 +1,19 @@
-from pathlib import Path
+import time
 
 from langchain_community.document_loaders import PyPDFLoader
 
 
 def load_pdf(pdf_path: str):
-    """
-    Loads a pdf and returs a list of langchain Documents
-    """
+    start = time.perf_counter()
+
     loader = PyPDFLoader(pdf_path)
+
+    print(f"Create Loader: {time.perf_counter() - start:.2f}s")
+
+    start = time.perf_counter()
+
     documents = loader.load()
+
+    print(f"Parse PDF: {time.perf_counter() - start:.2f}s")
 
     return documents
